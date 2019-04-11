e842fc8beede1371a239158e98257cc28768af42
lwq1030 <787998189@qq.com>
2019-04-11 10:03:54 +0800

    Initial Commit

---
+++ Cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cpp
+//
+//  Created by 李文祺 on 2019/4/11.
+//  Copyright © 2019 李文祺. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
