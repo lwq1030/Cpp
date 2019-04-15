152789480ffbf56df41218a6c8dc45d4bc7a6d90
lwq1030 <787998189@qq.com>
2019-04-15 14:33:20 +0800

    栈

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -7,3 +7,58 @@
 //
 
 #include "stack.hpp"
+#include <iostream>
+MyStack::MyStack(int size){
+    m_iSize=size;
+    m_pBuffer=new char[size];
+    m_iTop=0;//空栈
+}
+MyStack::~MyStack(){
+    delete[] m_pBuffer;
+    m_pBuffer=NULL;
+}
+bool MyStack::stackEmpty(){
+    if(m_iTop==0){     //if(0==m_iTop)
+        return true;
+    }else return false;
+    return 0;
+}
+bool MyStack::stackFull(){
+    if(m_iTop>=m_iSize){
+        return true;
+    }return false;
+}
+void MyStack::clearStack(){
+    m_iTop=0;
+}
+int MyStack::stackLength(){
+   return m_iTop;
+}
+bool MyStack::push(char elem){
+    if(stackFull()){
+        return false;
+    }
+  m_pBuffer[m_iTop]=elem;
+    m_iTop++; //永远指向空位置
+    return true;
+}
+bool MyStack::pop(char &elem){
+    if(stackEmpty()) return false;
+    m_iTop--;
+    elem=m_pBuffer[m_iTop];
+    return true;
+}
+/*
+ char MyStack::pop(){
+ if(stackEmpty()) return false;
+ else {
+ m_iTop--;
+ return m_pBuffer[m_iTop];
+ */
+
+void MyStack::stackTraverse(){
+    for(int i=0;i<m_iTop;i++){
+        std::cout<<m_pBuffer[i]<<",";
+        
+    }
+}
--- stack/stack.hpp
+++ stack/stack.hpp
@@ -10,5 +10,23 @@
 #define stack_hpp
 
 #include <stdio.h>
+class MyStack{
+public:
+    MyStack(int size);
+    ~MyStack();
+    bool stackEmpty();
+    bool stackFull();
+    void clearStack();
+    int stackLength();
+    bool push(char elem);
+    bool pop(char &elem);
+    void stackTraverse();
+private:
+    char *m_pBuffer; //指向栈空间，new空间并用指针指向。
+    int m_iSize;
+    int m_iTop; //栈顶，反应元素个数
+    
+};
+
 
 #endif /* stack_hpp */
