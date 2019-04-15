235635a125d492bd2f6502931e408ebd6d230c09
lwq1030 <787998189@qq.com>
2019-04-15 14:33:03 +0800

    demo

--- main.cpp
+++ main.cpp
@@ -9,12 +9,17 @@
 #include <stdio.h>
 #include"list.hpp"
 #include<iostream>
+#include "stack.hpp"
 using namespace std;
 int main(void){
     Node node1;
     node1.data=3;
     List *pList=new List();
     pList->ListInsertHead(&node1);
+    MyStack *pStack = new MyStack(5);
+    cout<<pStack->stackLength()<<endl;
+    if(pStack->stackEmpty()) cout<<"stack is empty"<<endl;
+    if(pStack->stackFull()) cout<<"Stack is full"<<endl;
     
     delete pList;
     pList=NULL;
