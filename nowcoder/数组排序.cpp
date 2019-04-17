aa110b5091e6a3a0dd271c61d1e571ba8d9f79d7
lwq1030 <787998189@qq.com>
2019-04-17 11:24:53 +0800

    数组排序

---
+++ nowcoder/数组排序.cpp
@@ -0,0 +1,35 @@
+//
+//  数组排序.cpp
+//  Cpp
+//
+//  Created by 李文祺 on 2019/4/17.
+//  Copyright © 2019 李文祺. All rights reserved.
+//
+
+#include <stdio.h>
+#include <vector>
+#include <math.h>
+using namespace std;
+class Solution{
+public:
+    int minNumberInRotateArray(vector<int> rotateArray){
+        sort(rotateArray.begin(),rotateArray.end());
+        return rotateArray[0];
+        
+    }
+};
+class Solution1{
+public:
+    int minNumberInRotateArray(vector<int> rotateArray){
+        int min=65536;
+        if(!rotateArray.empty()){
+            for (int i=0;i<rotateArray.size();i++){
+                if(min>rotateArray.at(i)){
+                    min=rotateArray.at(i);
+                }
+                return min;
+            }
+        }
+        else return 0;
+    }
+};
